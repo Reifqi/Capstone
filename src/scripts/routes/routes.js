6cc61e5c35e006f5ea57392eabec50470f401243
Reifqi <rifqi_m20@mhs.unsyiah.ac.id>
2022-11-27 14:34:22 +0700

    Adding cat pages
    Tinggal memecah bagian datanya.

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -2,12 +2,14 @@ import Detail from '../view/pages/detail';
 import Home from '../view/pages/home';
 import Cat from '../view/pages/cat';
 import Dog from '../view/pages/dog';
+import catHealth from '../view/pages/catHealth';
 
 const routes = {
   '/': Home, // default page
   '/home': Home,
   '/dog': Dog,
   '/cat': Cat,
+  '/cathealth': catHealth,
   '/detail/:id': Detail,
 };
 
---
+++ src/scripts/view/pages/catHealth.js
@@ -0,0 +1,14 @@
+const catHealth = {
+    async render() {
+      return `
+          <h2>Health Page(Cat)</h2>
+        `;
+    },
+  
+    afterRender() {
+      // Fungsi ini akan dipanggil setelah render()
+    },
+  };
+  
+  export default catHealth;
+  
