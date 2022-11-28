6b08ff33fd129cae5cd7e59f8f3c22b7c2f6e567
YanuarN <yanuaralfi@gmail.com>
2022-11-28 21:33:40 +0700

    merapikan page lingkungan dan menambahkan data

--- src/scripts/view/pages/lingkungan.js
+++ src/scripts/view/pages/lingkungan.js
@@ -1,44 +1,20 @@
-const Lingkungan = {
-    async render() {
-        return `
-        <main id="mainContent">
-            <div class="arkikel_suhu">
-                <h1>Kondisi Lingkungan Yang Harus Anda Perhatikan</h2>
-                <article id="lingkungan" class="lingkungan">
-<h2>Berapa Suhu Rumah Ideal untuk Anjing dan Kucing?</h1>
-<p>Anda suka memanjakan hewan peliharaan anda dengan mainan kesukaanya,alamt makan otomatis dan pintu hewan peliharaan otomatis. Tapi apakah Anda yakin anjing atau kucing Anda senang dengan pengaturan termostat? Suhu terbaik untuk hewan peliharaan Anda bergantung pada berbagai faktor termasuk ras, kondisi kesehatan, dan keadaan lainnya. Pertimbangkan faktor-faktor yang memengaruhi suhu ideal hewan peliharaan Anda dan cara memilih pengaturan termostat yang sempurna untuk menyeimbangkan kenyamanan dan efisiensi energi.</p>
-<h4>Faktor-Faktor yang Mempengaruhi Suhu Ideal Hewan Peliharaan</h4>
-<p>Tidak ada satu pun pengaturan suhu yang sempurna untuk mengakomodasi kebutuhan semua hewan peliharaan di setiap musim. Pertimbangkan faktor-faktor berikut untuk membantu Anda menentukan apakah hewan peliharaan Anda lebih menyukai rumah yang hangat atau sejuk:</p>
-<ul>
-    <li>
-        <p>Jenis bulu:&nbsp;Anjing dan kucing dengan bulu tebal dan panjang cenderung lebih toleran terhadap dingin. Bahkan, mereka sering kepanasan lebih cepat dari manusia. Di sisi lain, hewan peliharaan dengan bulu tipis dan pendek (dan varietas tidak berbulu) juga tidak dapat mempertahankan panas tubuh dan oleh karena itu lebih menyukai suhu yang lebih hangat.</p>
-    </li>
-    <li>
-        <p>Ukuran:&nbsp;Hewan peliharaan yang lebih kecil memiliki rasio luas permukaan terhadap volume yang lebih tinggi. Ini berarti mereka memiliki lebih banyak kulit untuk kehilangan panas tubuh relatif terhadap ukurannya daripada hewan yang lebih besar. Itu sebabnya anjing kecil cenderung lebih cepat kedinginan daripada ras besar.</p>
-    </li>
-    <li>
-        <p>Berat:&nbsp;Lemak tubuh juga merupakan faktor penyebab. Hewan peliharaan yang kelebihan berat badan memiliki lapisan isolasi yang lebih tebal untuk melindungi dari hawa dingin. Namun, risiko kesehatan yang terkait dengan obesitas jauh lebih besar daripada tambahan kehangatan di musim dingin, jadi usahakan agar hewan peliharaan Anda tetap ramping dan bugar.</p>
-    </li>
-    <li>
-        <p>Usia dan kesehatan:&nbsp;Anak anjing dan anak kucing yang masih kecil, serta hewan peliharaan yang sudah lanjut usia, cenderung membutuhkan suhu kandang yang lebih hangat. Hewan dengan infeksi akut atau kondisi persendian kronis mendapat manfaat dari suhu yang lebih tinggi dan area tidur yang panas juga.</p>
-    </li>
-</ul>
-<h4>Memilih Suhu Terbaik untuk Hewan Peliharaan</h4>
-<p>Dengan informasi di atas, Anda dapat mengatur suhu rumah ke pengaturan yang ideal untuk Anda dan hewan peliharaan Anda. Berikut rekomendasi kami:</p>
-<h4>Suhu Musim Panas</h4>
-<p>Secara umum, Anda harus menyetel termostat antara 23 dan 25 derajat saat Anda berada di rumah pada musim panas. Jika Anda memiliki anjing besar berbulu panjang, seperti Husky atau Samoyed, suhu sekitar 23 derajat mungkin yang terbaik. Jika Anda memiliki kucing tua berambut pendek, 25 derajat lebih disukai.</p>
-<p>Saat Anda pergi bekerja sepanjang hari atau berlibur, jangan atur suhu lebih tinggi dari 26 hingga 27 derajat. Untuk memastikan hewan peliharaan Anda tetap nyaman, sediakan air segar dan area untuk mendinginkannya, seperti ruang bawah tanah atau ruangan dengan lantai keramik.</p>
-<h4>Suhu Musim Dingin</h4>
-<p>Rencanakan untuk menyetel termostat antara 20 dan 22 derajat saat Anda berada di rumah pada musim dingin. Hewan besar dengan bulu tebal cenderung menyukai suhu di ujung bawah spektrum ini, sementara Anda mungkin ingin menyetel termostat lebih tinggi untuk kucing atau anak anjing Anda yang tidak berbulu.</p>
-<p>Saat Anda pergi, setel termostat tidak lebih rendah dari 15 derajat. Pertahankan suhu sedikit lebih tinggi untuk hewan kecil, muda, atau sakit. Selalu berikan hewan peliharaan Anda tempat tidur yang lembut dan hangat, dan buka tirai agar sinar matahari masuk, beri hewan tempat untuk berjemur.</p>
-</article>
-            </div> 
-        </main>
-        `;
-    },
+import createLingPage from '../template/lingkungan-template';
+import dataLing from '../../data/lingkungan-data';
 
-    async afterRender() {
-      },
-    };
+const Lingkungan = {
+  render() {
+    return `
+    <div class="latest">
+      <h1 class="latest__label">Perhatikan Lingkungan Sekitar Untuk Hewan Anda</h1>
+      <div class="posts"></div>
+    </div>
+        `;
+  },
+
+  async afterRender() {
+    const lingContainer = document.querySelector('.posts');
+    lingContainer.innerHTML += createLingPage(dataLing);
+  },
+};
 
 export default Lingkungan;
