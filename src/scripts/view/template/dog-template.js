ce116c072d85e43a11067a1af8fbbb551bc2b779
Reifqi <rifqi_m20@mhs.unsyiah.ac.id>
2022-12-02 18:13:17 +0700

    Added cat type page with api and improve css in dog page
    Memperbagus halaman anjing dan menambah api dengan fitur search pada halamaan jenis anjing

--- src/scripts/view/template/dog-template.js
+++ src/scripts/view/template/dog-template.js
@@ -3,7 +3,7 @@ const createDogTemp1 = `
                 <div class="card dog-card-1">
                   <div class="card-body">
                     <h5 class="card-title">Kesehatan</h5>
-                    <p class="card-text dog-title">Dengan membuat kesehatan anjing terjaga maka akan membuat sang pemilik juga sehat dari ancaman penyakit, maka dari itu pemiliki harus menjaganya. Halaman berikut akan berisi tentang segala informasi terhadap panduan untuk menjaga kesehatan anjing, tentu saja dilengkapi dengan pencegahannya</p>
+                    <p class="card-text dog-title">Dengan membuat kesehatan anjing terjaga maka akan membuat sang pemilik juga sehat dari ancaman penyakit. Halaman berikut akan berisi tentang segala informasi terhadap panduan untuk menjaga kesehatan anjing, tentu saja dilengkapi dengan pencegahannya</p>
                     <a href="#/dogHealth" class="btn btn-primary">Detail Selengkapnya...</a>
                   </div>
                 </div>
