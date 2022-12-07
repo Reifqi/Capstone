7c5e625371c101def01d94477b2d22304eaaa13a
Reifqi <rifqi_m20@mhs.unsyiah.ac.id>
2022-12-07 23:31:56 +0700

    Update dogHealth.js
    memperbaiki kesalah penulisan template

--- src/scripts/view/pages/dogHealth.js
+++ src/scripts/view/pages/dogHealth.js
@@ -1,16 +1,16 @@
 import createDogHealthTemp from '../template/dog-health-template';
 
 const dogHealth = {
-    async render() {
-        return `
+  async render() {
+    return `
     <div class='dog-health-page'></div>
    `;
-    },
+  },
 
-    async afterRender() {
-        const dogHealthContainer = document.querySelector('.dog-health-page');
-        dogHealthContainer.innerHTML += createdogHealthTemp;
-    },
+  async afterRender() {
+    const dogHealthContainer = document.querySelector('.dog-health-page');
+    dogHealthContainer.innerHTML += createDogHealthTemp;
+  },
 };
 
 export default dogHealth;
